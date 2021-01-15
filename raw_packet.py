f8855ef75439b3799486e5ec53f86a1cae79ba12
0yah <highmeme10@gmail.com>
2021-01-15 13:32:21 +0300

    Packet

---
+++ raw_packet.py
@@ -0,0 +1,19 @@
+"""
+
+Protocol Header Dictonary
+
+Informs the receiver about the content in the payload of the message
+Arbitary data can be sent while providing enough information so the content can be decoded and interpreted correctly by the receiver
+
+
+byteorder
+content-length
+
+content-type
+    text/json
+    binary/my-custom-binary-type
+
+content-encoding
+    utf-8
+
+"""
