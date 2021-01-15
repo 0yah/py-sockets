fcf5d284e21a914b180ffa1954b79c6b76d29be5
0yah <highmeme10@gmail.com>
2021-01-15 13:32:55 +0300

    Packet

--- raw_packet.py
+++ raw_packet.py
@@ -5,7 +5,6 @@ Protocol Header Dictonary
 Informs the receiver about the content in the payload of the message
 Arbitary data can be sent while providing enough information so the content can be decoded and interpreted correctly by the receiver
 
-
 byteorder
 content-length
 
